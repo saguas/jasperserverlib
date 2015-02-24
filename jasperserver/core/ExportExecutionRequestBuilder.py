714815fd943207089c1805e01c4f476ddf7c6917
Luis Fernandes <luisfmfernandes@gmail.com>
2015-02-24 19:51:01 +0000

    Add suport for html reports

--- jasperserver/core/ExportExecutionRequestBuilder.py
+++ jasperserver/core/ExportExecutionRequestBuilder.py
@@ -16,31 +16,34 @@ class ExportExecutionRequestBuilder(object):
     
     #for each id. Get one file each time
     def outputResource(self):
-            #exports = self.opresult.get('exports', [])
-            path = "/%s/exports/%s/outputResource" % (self.requestId, self.exportId)
-            content = None
-            while True:
-                response = self._connect.get(self.url + path).response
-                print "output-final {}".format(response.headers['output-final'])
-                if response.headers['output-final'] == "true":
-                    break
-        
-            #result = response.content
-            self.result["content"] = response.content
-        
-            return self
+        #exports = self.opresult.get('exports', [])
+        limit = 10
+        path = "/%s/exports/%s/outputResource" % (self.requestId, self.exportId)
+        content = None
+        while limit > 0:
+            response = self._connect.get(self.url + path).response
+            print "output-final {}".format(response.headers['output-final'])
+            if response.headers['output-final'] == "true":
+                break
+            limit = limit - 1
+    
+        #result = response.content
+        self.result["content"] = response.content
+    
+        return self
         
     def attachment(self, attachmentId):
         if not attachmentId or attachmentId == "/":
             raise Exception("attachmentId mustn't be an empty string!")
-            
+        limit = 10
         path = "/%s/exports/%s/attachments/%s" % (self.requestId, self.exportId, attachmentId)
-        while True:
+        while limit > 0:
             result = self.rerb.status().content
             status = json.loads(result)
             if status.get('value') == "ready":
                 break
             sleep(1)
+            limit = limit - 1
         print "before pedido attachment {} path {} conn {}".format(attachmentId, self.url + path, self._connect)
         response = self._connect.get(self.url + path)
         #print "path attach 5 {} content {}".format(self.url + path, response.content)
